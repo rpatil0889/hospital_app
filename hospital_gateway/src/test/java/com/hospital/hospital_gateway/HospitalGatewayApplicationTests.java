50fff531f237b3382619fb4f2a56da4778fded31
Rohit Patil <rpatil0889@gmail.com>
2025-06-21 00:23:50 +0530

    gate-way impl

---
+++ hospital_gateway/src/test/java/com/hospital/hospital_gateway/HospitalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
